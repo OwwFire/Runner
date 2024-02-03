525b3f8394d056bf9cdd5531d8b123869c3361b8
OwwFire <itayash1002@gmail.com>
2024-02-03 21:58:48 +0200

    jumping

--- Code/Js/main.js
+++ Code/Js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-    const obstacle = $('.charactes-container').append(spikesObstacle);
+    addObstacle(spikesObstacle);
+
+    //Jump functionlity
+    let jumpFlag = false;
+    $(document).on
+        ('keydown', (event) => {
+            if (!jumpFlag &&
+                event.key !== 'ArrowUp' &&
+                event.key !== ' ') return;
+            $('.runner-character').animate({
+                marginBottom: '15vh',
+                alignSelf: "flex-end"
+            }, 500)
+            jumpFlag = true;
+            setTimeout(() => {
+                $('.runner-character').animate({
+                    marginBottom: '0',
+                    alignSelf: "flex-end"
+                }, 500)
+                jumpFlag = false;
+            }, 500)
+        });
 });
--- Code/Js/obstacles.js
+++ Code/Js/obstacles.js
@@ -1,22 +1,29 @@
+
 const spikesObstacle = $("<img>")
     .attr({
         src: "\\Images\\Obstacles\\Spikes.png",
         alt: "Spikes"
     }).css({
-        height: "6vh",
+        height: "7vh",
         width: "3vw"
     });
-
 const addCssPositioning = (object) => {
     object.css({
         marginLeft: "100%",
         alignSelf: "flex-end"
     });
-}
+};
 
 let movingTime = 5000;
 const addMovingAnimation = (object) => {
     object.animate({
         marginLeft: "-20%"
     }, movingTime);
+};
+
+function addObstacle(object) {
+    const DuplicateObject = object.clone();
+    addCssPositioning(DuplicateObject);
+    addMovingAnimation(DuplicateObject);
+    $('.charactes-container').append(DuplicateObject);
 };
